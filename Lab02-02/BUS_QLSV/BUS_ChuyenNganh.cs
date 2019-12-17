167c400855336d21365fb4c0c96ff87836e71e27
DUONG THANH TOAN <DUONG THANH TOAN@DESKTOP-IN0VR6B>
2019-12-17 10:21:00 +0700

    Lab02-02

---
+++ Lab02-02/BUS_QLSV/BUS_ChuyenNganh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_QLSV;
+
+namespace BUS_QLSV
+{
+    public class BUS_ChuyenNganh
+    {
+        QuanLySinhVienEntities1 dbContext = new QuanLySinhVienEntities1();
+
+        public BUS_ChuyenNganh()
+        {
+
+        }
+        public List<ChuyenNganh> GetChuyenNganh()
+        {
+            return dbContext.ChuyenNganhs.ToList();
+
+        }
+    }
+}
---
+++ Lab02-02/Lab02-02/frmQuanLySinhVien.cs
@@ -0,0 +1,41 @@
+﻿using BUS_QLSV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_02
+{
+   //
+    public partial class frmQuanLySinhVien : Form
+    {
+        BUS_ChuyenNganh bus_chuyenNganh = new BUS_ChuyenNganh();
+        public frmQuanLySinhVien()
+        {
+            InitializeComponent();
+            ControlConfig();
+        }
+
+        private void ControlConfig()
+        {
+            cmbChuyenNganh.DisplayMember = "TenChuyenNganh";
+            cmbChuyenNganh.ValueMember = "MaChuyenNgnah";
+        }
+
+        private void frmQuanLySinhVien_Load(object sender, EventArgs e)
+        {
+            LoadChuyenNganh();
+        }
+
+        private void LoadChuyenNganh()
+        {
+            cmbChuyenNganh.DataSource = bus_chuyenNganh.GetChuyenNganh();
+        
+        }
+    }
+}
